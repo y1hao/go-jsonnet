fe1d35d61e30c8890c9c4fe18782e65cb8e8a41d
Andrea Scarpino <andrea@scarpino.dev>
2024-07-29 09:08:08 +0200

    Fix compatibility with setuptools 72

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ import os
 from setuptools import setup
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext as BuildExt
-from setuptools.command.test import test as TestCommand
 from subprocess import Popen, PIPE
 
 DIR = os.path.abspath(os.path.dirname(__file__))
@@ -52,14 +51,6 @@ class BuildJsonnetExt(BuildExt):
 
         BuildExt.run(self)
 
-
-class NoopTestCommand(TestCommand):
-    def __init__(self, dist):
-        print(
-            "_gojsonnet does not support running tests with 'python setup.py test'. Please run 'pytest'."
-        )
-
-
 jsonnet_ext = Extension(
     "_gojsonnet",
     sources=MODULE_SOURCES,
@@ -79,7 +70,6 @@ setup(
     version=get_version(),
     cmdclass={
         "build_ext": BuildJsonnetExt,
-        "test": NoopTestCommand,
     },
     ext_modules=[jsonnet_ext],
 )
